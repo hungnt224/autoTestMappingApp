9bf4efeca25297083951edece7500a83c7bdb5cc
hungnt224 <132182723+hungnt224@users.noreply.github.com>
2026-02-25 22:15:55 +0700

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,34 +1,18 @@
 pipeline {
     agent { label 'automationtest' }
 
-    environment {
-        PROJECT_PATH = 'C:\\resourcetest'
-    }
-
     stages {
-
-        stage('Checkout Source') {
-            steps {
-                git branch: 'main',
-                    credentialsId: 'github-creds',   // nếu repo private
-                    url: 'https://github.com/hungnt224/autoTestMappingApp.git'
-            }
-        }
-
         stage('Build & Run Test') {
             steps {
-                bat 'mvn clean test'
+                dir('C:/Users/HungNT/Desktop/seminar_t5/mappingApp/autoTestMappingApp')
+                {bat 'mvn clean test'}
             }
         }
     }
 
     post {
         always {
-            allure(
-                includeProperties: false,
-                jdk: '',
-                results: [[path: 'target/allure-results']]
-            )
+            allure results: [[path: 'C:/Users/HungNT/Desktop/seminar_t5/mappingApp/autoTestMappingApp/target/allure-result']]
     }
 }
 }
